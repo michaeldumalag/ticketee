d12c790ac241db8890c0f38ed95462b5075c18b0
michaeldumalag <michaeldumalag@gmail.com>
2017-02-08 22:37:17 -0800

    Set strong parameters for project creation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,20 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
